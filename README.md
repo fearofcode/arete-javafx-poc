60a44c72e79b8b9ff37cb2cb5b307fef3c626bdf
Warren Henning <warren.henning@gmail.com>
2018-08-18 00:21:52 -0700

    Got simple Postgres querying going.

--- README.md
+++ README.md
@@ -5,6 +5,15 @@ Experimental project to try to practice programming via spaced repetition.
 
 We'll see how it goes.
 
+Setup
+=====
+
+- Make a PostgreSQL database called `arete`, user `arete`, password `arete`
+- Configure PostgreSQL however you like:
+    - `shared_buffers = 4GB`
+    - `random_page_cost = 1.5`
+    - etc
+
 LICENSE
 =======
 
--- src/main/java/org/wkh/arete/AreteApplication.java
+++ src/main/java/org/wkh/arete/AreteApplication.java
@@ -1,12 +1,61 @@
 package org.wkh.arete;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 @SpringBootApplication
-public class AreteApplication {
+public class AreteApplication implements CommandLineRunner {
+    private static final Logger log = LoggerFactory.getLogger(AreteApplication.class);
 
-	public static void main(String[] args) {
-		SpringApplication.run(AreteApplication.class, args);
-	}
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    public static void main(String args[]) {
+        SpringApplication.run(AreteApplication.class, args);
+    }
+
+    @Override
+    public void run(String... strings) {
+        log.info("Creating tables");
+
+        jdbcTemplate.execute("DROP TABLE IF EXISTS customers");
+        jdbcTemplate.execute("CREATE TABLE customers(" +
+                "id SERIAL, first_name VARCHAR(255), last_name VARCHAR(255))");
+        jdbcTemplate.execute("create index first_name_index on customers(first_name)");
+
+        log.info("Done with DDL stuff");
+
+        // Split up the array of whole names into an array of first/last names
+        final List<Object[]> splitUpNames = Stream.of("John Woo", "Jeff Dean", "Josh Bloch", "Josh Long")
+                .map(name -> name.split(" "))
+                .collect(Collectors.toList());
+
+        final String sql = "INSERT INTO customers(first_name, last_name) VALUES (?,?)";
+
+        log.info("about to insert");
+        jdbcTemplate.batchUpdate(sql, splitUpNames);
+
+        log.info("Done inserting. Querying for customer records where first_name = 'Josh':");
+
+        jdbcTemplate.query(
+                "SELECT id, first_name, last_name FROM customers WHERE first_name = ?",
+                new Object[]{"Josh"},
+                (rs, rowNum) -> new Customer(
+                        rs.getLong("id"),
+                        rs.getString("first_name"),
+                        rs.getString("last_name")
+                )
+        ).forEach(customer -> log.info(customer.toString()));
+
+        log.info("Done with everything");
+    }
 }
