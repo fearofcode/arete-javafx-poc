ca3d51173c53247a9fc2580b64f2fff4ff4d01f1
Warren Henning <warren.henning@gmail.com>
2018-08-19 12:41:57 -0700

    Got interactive demo of jLatexMath working

--- src/main/java/org/wkh/arete/LatexDemoController.java
+++ src/main/java/org/wkh/arete/LatexDemoController.java
@@ -1,5 +1,6 @@
 package org.wkh.arete;
 
+import javafx.concurrent.Task;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
@@ -8,6 +9,7 @@ import javafx.scene.control.TextArea;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.embed.swing.SwingFXUtils;
+import org.scilab.forge.jlatexmath.ParseException;
 import org.scilab.forge.jlatexmath.TeXConstants;
 import org.scilab.forge.jlatexmath.TeXFormula;
 import org.scilab.forge.jlatexmath.TeXIcon;
@@ -19,9 +21,10 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.File;
-import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 @Controller
 public class LatexDemoController implements Initializable {
@@ -32,59 +35,76 @@ public class LatexDemoController implements Initializable {
     @FXML public TextArea commandConsoleOutput;
     @FXML public ImageView createdImage;
 
+    private ExecutorService threadPool;
     private final int FONT_SIZE = 24;
 
-    public void generateLatex(ActionEvent actionEvent) throws IOException, InterruptedException {
-        log.info("Starting latex processing");
+    public void generateLatex(ActionEvent actionEvent) {
         String latexInput = markupInput.getText().trim();
 
         processWithMiktex(latexInput);
-        //processWithJLatexMath(latexInput);
-        log.info("Image set");
     }
 
     private void processWithJLatexMath(String latexInput) {
         SwingUtilities.invokeLater(() -> {
-            TeXFormula formula = new TeXFormula(latexInput);
-            TeXIcon icon = formula.createTeXIcon(TeXConstants.STYLE_DISPLAY, FONT_SIZE);
-            icon.setInsets(new Insets(2, 2, 2, 2));
-            int w = icon.getIconWidth(), h = icon.getIconHeight();
+            try {
+                TeXFormula formula = new TeXFormula(latexInput);
+                TeXIcon icon = formula.createTeXIcon(TeXConstants.STYLE_DISPLAY, FONT_SIZE);
+                icon.setInsets(new Insets(2, 2, 2, 2));
+                int w = icon.getIconWidth(), h = icon.getIconHeight();
 
-            BufferedImage bufferedImage = new BufferedImage(w, h, BufferedImage.TYPE_INT_ARGB);
-            Graphics2D g2 = bufferedImage.createGraphics();
+                BufferedImage bufferedImage = new BufferedImage(w, h, BufferedImage.TYPE_INT_ARGB);
+                Graphics2D g2 = bufferedImage.createGraphics();
 
-            icon.setForeground(Color.BLACK);
-            icon.paintIcon(null, g2, 0, 0);
-            g2.dispose();
+                icon.setForeground(Color.BLACK);
+                icon.paintIcon(null, g2, 0, 0);
+                g2.dispose();
 
-            Image image = SwingFXUtils.toFXImage(bufferedImage, null);
-            createdImage.setImage(image);
+                Image image = SwingFXUtils.toFXImage(bufferedImage, null);
+                createdImage.setImage(image);
+            } catch (ParseException pe) {
+                createdImage.setImage(null);
+            }
+
         });
     }
 
-    private void processWithMiktex(String latexInput) throws IOException, InterruptedException {
-        /* this is super shitty and needs to be converted to a background task */
-        /* I will, I just want to get it to work */
+    private void processWithMiktex(String latexInput) {
+        final Task<Boolean> processingTask = new Task<>() {
+            @Override
+            protected Boolean call() throws Exception {
+                final MiktexLatexCommandPOC processor = new MiktexLatexCommandPOC(latexInput, FONT_SIZE);
+                String pngPath = processor.process();
 
-        final MiktexLatexCommandPOC processor = new MiktexLatexCommandPOC(latexInput, FONT_SIZE);
-        String pngPath = processor.process();
-
-        if (pngPath != null) {
-            // TODO again make this non-blocking
-            File imageFile = new File(pngPath);
-            String uriString = imageFile.toURI().toString();
-            Image pngImage = new Image(uriString);
-            createdImage.setImage(pngImage);
-        } else {
-            commandConsoleOutput.appendText("Processing failed, see terminal for diagnostic information");
-        }
+                if (pngPath != null) {
+                    File imageFile = new File(pngPath);
+                    String uriString = imageFile.toURI().toString();
+                    Image pngImage = new Image(uriString, true);
+                    createdImage.setImage(pngImage);
+                    commandConsoleOutput.appendText("Image processed");
+                    return true;
+                } else {
+                    commandConsoleOutput.appendText("Processing failed, see terminal for diagnostic information");
+                    return false;
+                }
+            }
+        };
+        threadPool.submit(processingTask);
     }
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+        threadPool = Executors.newFixedThreadPool(5);
         markupInput.textProperty().addListener((observable, oldValue, newValue) -> {
             final String trimmed = newValue.trim();
-            processInputButton.disableProperty().set(trimmed.length() == 0);
+            final boolean validInput = trimmed.length() != 0;
+            processInputButton.disableProperty().set(!validInput);
+
+            if (validInput) {
+                String wrappedFormula = String.format("\\textrm{%s}", trimmed);
+                processWithJLatexMath(wrappedFormula);
+            } else {
+                createdImage.setImage(null);
+            }
         });
     }
 }
